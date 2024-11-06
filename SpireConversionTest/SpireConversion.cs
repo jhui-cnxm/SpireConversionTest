a576229093eeca18bdaa97da6898465911875d8d
ECMARKET\jhui <jhui@conexiom.com>
2024-11-06 13:43:54 -0800

    Add project files.

---
+++ SpireConversionTest/SpireConversion.cs
@@ -0,0 +1,41 @@
+﻿using Spire.Pdf;
+
+namespace SpireConversionTest
+{
+    public class SpireConversion
+    {
+        public SpireConversion()
+        {
+            Spire.Doc.License.LicenseProvider.SetLicenseKey("sample_license_key");
+            Spire.Pdf.License.LicenseProvider.SetLicenseKey("sample_license_key");
+        }
+
+        public PdfDocument ExecuteWorkflow()
+        {
+            string extension = "doc";
+            byte[] docData = File.ReadAllBytes($"..\\..\\..\\..\\document.{extension}");
+
+            // Load DOC document
+            Spire.Doc.FileFormat format = Spire.Doc.FileFormat.Auto;
+            var document = new Spire.Doc.Document();
+            using (var inStream = new MemoryStream(docData))
+            {
+                document.LoadFromStream(inStream, format);
+            }
+
+            // Extract PDF byte data
+            byte[] pdfData;
+            using (var outStream = new MemoryStream())
+            {
+                document.SaveToStream(outStream, Spire.Doc.FileFormat.PDF);
+                outStream.Position = 0;
+                pdfData = outStream.ToArray();
+            }
+
+            // Create PDF document from byte data
+            var pdfDocument = new PdfDocument();
+            pdfDocument.LoadFromBytes(pdfData);
+            return pdfDocument;
+        }
+    }
+}
---
+++ SpireConversionTest/SpireConversionTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace SpireConversionTest
+{
+    public class SpireConversionTests
+    {
+        private SpireConversion spireConversion = new SpireConversion();
+
+        [Fact]
+        public void SpireConversionTest()
+        {
+            var pdfDoc1 = spireConversion.ExecuteWorkflow();
+            var pdfDoc2 = spireConversion.ExecuteWorkflow();
+
+            // compare objects
+            //pdfDoc1.Should().BeEquivalentTo(pdfDoc2);     // Test fails here
+
+            // compare streams
+            var stream1 = pdfDoc1.SaveToStream(Spire.Pdf.FileFormat.SVG);
+            string streamTxt1 = "";
+            foreach (var stream in stream1)
+            {
+                StreamReader reader = new StreamReader(stream);
+                streamTxt1 += reader.ReadToEnd();
+            }
+
+            var stream2 = pdfDoc2.SaveToStream(Spire.Pdf.FileFormat.SVG);
+            string streamTxt2 = "";
+            foreach (var stream in stream2)
+            {
+                StreamReader reader = new StreamReader(stream);
+                streamTxt2 += reader.ReadToEnd();
+            }
+
+            streamTxt1.Should().Be(streamTxt2);         // Test fails here
+        }
+    }
+}
